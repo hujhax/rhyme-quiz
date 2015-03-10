440f286627f6a9569407af302a90e860a2514d8b
Peter Rogers <axyaqehsmq@snkmail.com>
2015-03-10 15:42:46 -0500

    Show proper rhyme data on startup.  Use a session variable for now.

--- client/quiz.js
+++ client/quiz.js
@@ -3,9 +3,40 @@ Template.quiz.initialize = function() {
 
     var curRhyme = _.sample(rhymeData);
     var rhymeSetString = curRhyme && curRhyme.words;
-    var rhymeSet = rhymeSetString.split(/\s*,\s*/); // split on commas and trim whitespace
-}
 
-Template.quiz.created = function() {
+    if (!rhymeSetString) {
+        return;
+    }
+
+    var rhymeSet = rhymeSetString.split(/\s*,\s*/); // split on commas and trim whitespace
+    var rhymeWord = _.sample(rhymeSet);
+    rhymeSet = _.without(rhymeSet, rhymeWord);
+
+    Session.set("rightAnswers", []);
+    Session.set("wrongAnswers", []);
+    Session.set("otherAnswers", rhymeSet);
+    Session.set("rhymeWord", rhymeWord);
+};
+
+Template.quiz.rendered = function() {
     Template.quiz.initialize();
-}
+};
+
+Template.quiz.helpers({
+    answers: function() {
+        return {
+            right: Session.get("rightAnswers"),
+            wrong: Session.get("wrongAnswers"),
+            other: Session.get("otherAnswers")
+        }
+    },
+    rhymeWord: function() {
+        return Session.get("rhymeWord");
+    }
+});
+
+Template.quiz.events({
+    'click .refresh': function() {
+        Template.quiz.initialize();
+    }
+});
